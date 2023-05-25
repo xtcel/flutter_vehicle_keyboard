63463126e27ab5c7663602e114fcf5c5c7a9eb85
zhangyong <yong.zhang@lelegege.com>
2023-05-25 16:25:30 +0800

    update .gitignore

--- example/android/app/src/main/java/com/lele/flutter_vehicle_keyboard_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.lele.flutter_vehicle_keyboard_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- ios/Classes/FlutterVehicleKeyboardPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterVehicleKeyboardPlugin : NSObject<FlutterPlugin>
-@end
